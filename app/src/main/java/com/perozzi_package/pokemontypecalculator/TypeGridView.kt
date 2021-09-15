186fe497c77045936ff4a2b09f98dc62049012c5
NickPerozzi <53713045+NickPerozzi@users.noreply.github.com>
2021-09-14 22:59:29 -0500

    -Changed com.example to com.perozzi_package

---
+++ app/src/main/java/com/perozzi_package/pokemontypecalculator/TypeGridView.kt
@@ -0,0 +1,17 @@
+package com.perozzi_package.pokemontypecalculator
+
+class TypeGridView {
+
+    var iconsInGridView: Int?
+    var textInGridView: Double?
+    var backgroundColorInGridView: Int?
+    var textColorInGridView: Int?
+
+    constructor(iconsInGridView: Int?, textInGridView: Double?, backgroundColorInGridView: Int?, textColorInGridView: Int?) {
+        this.iconsInGridView = iconsInGridView
+        this.textInGridView = textInGridView
+        this.backgroundColorInGridView = backgroundColorInGridView
+        this.textColorInGridView = textColorInGridView
+    }
+
+}
---
+++ app/src/main/java/com/perozzi_package/pokemontypecalculator/TypeMatchups.kt
@@ -0,0 +1,32 @@
+package com.perozzi_package.pokemontypecalculator
+
+enum class PokemonType(val type: String) {
+    Bug("Bug"),
+    Dark("Dark"),
+    Dragon("Dragon"),
+    Electric("Electric"),
+    Fairy("Fairy"),
+    Fighting("Fighting"),
+    Fire("Fire"),
+    Flying("Flying"),
+    Ghost("Ghost"),
+    Grass("Grass"),
+    Ground("Ground"),
+    Ice("Ice"),
+    Normal("Normal"),
+    Poison("Poison"),
+    Psychic("Psychic"),
+    Rock("Rock"),
+    Steel("Steel"),
+    Water("Water")
+}
+
+enum class Effectiveness (val impact: String) {
+    DOES_NOT_EFFECT("DNE"),
+    ULTRA_NOT_VERY_EFFECTIVE("UNVE"),
+    NOT_VERY_EFFECTIVE("NVE"),
+    EFFECTIVE("E"),
+    SUPER_EFFECTIVE("SE"),
+    ULTRA_SUPER_EFFECTIVE("USE"),
+    ULTRA_DOES_NOT_EFFECT("UDNE")
+}
