aa851990859106222d852c533355d414b0f1c292
NickPerozzi <53713045+NickPerozzi@users.noreply.github.com>
2021-09-17 13:41:41 -0500

    Fixed more of defending with dual typing

--- app/src/main/java/com/perozzi_package/pokemontypecalculator/TypeGrid.kt
+++ app/src/main/java/com/perozzi_package/pokemontypecalculator/TypeGrid.kt
@@ -2,7 +2,7 @@ package com.perozzi_package.pokemontypecalculator
 
 data class TypeGrid(
     var iconsInGridView: Int?,
-    var textInGridView: Double?,
+    var textInGridView: String?,
     var backgroundColorInGridView: Int?,
     var textColorInGridView: Int?
 )
