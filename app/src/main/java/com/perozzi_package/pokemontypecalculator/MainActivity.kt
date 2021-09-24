b545274d9adf7a6cce06bcd890937da77e040a8b
NickPerozzi <53713045+NickPerozzi@users.noreply.github.com>
2021-09-23 20:00:29 -0500

    a lot of cleaning up of code

--- app/src/main/java/com/perozzi_package/pokemontypecalculator/MainActivity.kt
+++ app/src/main/java/com/perozzi_package/pokemontypecalculator/MainActivity.kt
@@ -36,6 +36,13 @@ class MainActivity : AppCompatActivity() {
     var defendingType1 = "(choose)"
     var defendingType2 = "[none]"
 
+    // needed for makeDataVisibleIfATypeIsSelected()
+    private lateinit var typeTableRecyclerView: RecyclerView
+    private lateinit var gameSwitchText: TextView
+    private lateinit var gameSwitch: Switch
+    private lateinit var iceJiceSwitch: Switch
+    private lateinit var tableHeader: TextView
+
     @SuppressLint("UseSwitchCompatOrMaterialCode", "ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +53,7 @@ class MainActivity : AppCompatActivity() {
             ActivityMainBinding.inflate(layoutInflater); setContentView(binding.root) // Sets up bindings for activity_main
         // Night mode compatibility
         val mainLinearLayout = binding.mainLinearLayout
-        val gameSwitchText = binding.gameSwitchText
+        gameSwitchText = binding.gameSwitchText
         when (this.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
             Configuration.UI_MODE_NIGHT_YES -> {
                 mainLinearLayout.background =
@@ -64,15 +71,15 @@ class MainActivity : AppCompatActivity() {
         val defendingType2Spinner = binding.type2Spinner
         val attackingTypeSpinner = binding.attackingTypeSpinner
         val typeSelectionPrompt = binding.secondPrompt
-        val tableHeader = binding.tableHeader
+        tableHeader = binding.tableHeader
         val doesNotExistDisclaimer = binding.doesNotExistDisclaimer
         val attackingTypeSpinnerAndLabel = binding.attackingTypeSpinnerAndLabel
         val defendingType1SpinnerAndLabel = binding.defendingType1SpinnerAndLabel
         val defendingType2SpinnerAndLabel = binding.defendingType2SpinnerAndLabel
         val povSwitch = binding.povSwitch
-        val gameSwitch = binding.gameSwitch
-        val iceJiceSwitch = binding.iceJiceSwitch
-        val typeTableRecyclerView = binding.typeTableRecyclerView
+        gameSwitch = binding.gameSwitch
+        iceJiceSwitch = binding.iceJiceSwitch
+        typeTableRecyclerView = binding.typeTableRecyclerView
         val infoButton = binding.infoButton
 
         // UI
@@ -113,70 +120,40 @@ class MainActivity : AppCompatActivity() {
         // UI
         povSwitch.setOnCheckedChangeListener { _, onSwitch ->
             weAreDefending = onSwitch
-            when (weAreDefending) {
-                false -> {
+            // Swaps the respective spinners in and out
+            attackingTypeSpinnerAndLabel.visibility = if (weAreDefending) { View.GONE } else { View.VISIBLE }
+            defendingType1SpinnerAndLabel.visibility = if (weAreDefending) { View.VISIBLE } else { View.GONE }
+            defendingType2SpinnerAndLabel.visibility = if (weAreDefending) { View.VISIBLE } else { View.GONE }
 
-                    // Switch the spinners
-                    attackingTypeSpinnerAndLabel.visibility = View.VISIBLE
-                    defendingType1SpinnerAndLabel.visibility = View.GONE
-                    defendingType2SpinnerAndLabel.visibility = View.GONE
+            povSwitch.text = if (weAreDefending) { getString(R.string.pov_switch_to_defending) } else { getString(R.string.pov_switch_to_attacking) }
+            typeSelectionPrompt.text = if (weAreDefending) {resources.getString(R.string.defending_prompt) } else { resources.getString(R.string.attacking_prompt) }
 
-                    // transfer the value from the to-be-invisible spinner to the to-be-visible one
-                    if (defendingType1 != "(choose)") {
-                        attackingType = defendingType1
-                        attackingTypeSpinner.setSelection(defSpinner1Index)
-                    }
-                    if (defendingType1 == "(choose)" && defendingType2 != "[none]") {
-                        attackingType = defendingType2
-                        attackingTypeSpinner.setSelection(defSpinner2Index)
-                    }
-
-                    povSwitch.text = getString(R.string.pov_switch_to_attacking)
-                    typeSelectionPrompt.text = resources.getString(R.string.attacking_prompt)
-                    adjustTableHeaderText(tableHeader,attackingType)
-                    doesNotExistDisclaimer.visibility = View.INVISIBLE
-
-                    refreshTheData()
-
-/*
-                    mainActivityViewModel.listOfInteractions = mainActivityViewModel.attackingEffectivenessCalculator(attackingType)
-*/
+            // QoL: transfers the value from the to-be-invisible spinner to the to-be-visible one
+            if (weAreDefending) {
+                defendingType2 = "[none]"
+                defendingType2Spinner.setSelection(0)
+                defendingType1 = attackingType
+                defendingType1Spinner.setSelection(atkSpinnerIndex)
+            } else {
+                attackingType = defendingType1
+                attackingTypeSpinner.setSelection(defSpinner1Index)
+                if ((defendingType1 == "(choose)" || defendingType1 == Types.NoType.type) &&
+                    (defendingType2 != "[none]" && defendingType2 != Types.NoType.type)) {
+                    attackingType = defendingType2
+                    attackingTypeSpinner.setSelection(defSpinner2Index)
                 }
-                true -> {
-
-                    // Switch the spinners
-                    attackingTypeSpinnerAndLabel.visibility = View.GONE
-                    defendingType1SpinnerAndLabel.visibility = View.VISIBLE
-                    defendingType2SpinnerAndLabel.visibility = View.VISIBLE
-
-                    defendingType2 = "[none]"
-                    defendingType2Spinner.setSelection(0)
-                    defendingType1 = attackingType
-                    defendingType1Spinner.setSelection(atkSpinnerIndex)
-                    povSwitch.text = getString(R.string.pov_switch_to_defending)
-                    typeSelectionPrompt.text = resources.getString(R.string.defending_prompt)
-                    adjustTableHeaderText(tableHeader,defendingType1,defendingType2)
-
-                    refreshTheData()
-                }
+                doesNotExistDisclaimer.visibility = View.INVISIBLE
             }
+            refreshTheData()
+            adjustTableHeaderText()
         }
 
         attackingTypeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 atkSpinnerIndex = p2
                 attackingType = Types.values()[p2].type
-
-                // Table header text adjustment
-                adjustTableHeaderText(tableHeader,attackingType)
-
-                // Adjusts visibility depending on whether user has selected a type yet
-                makeVisibleIfTypeSelected(typeTableRecyclerView,attackingType)
-                makeVisibleIfTypeSelected(gameSwitchText,attackingType)
-                makeVisibleIfTypeSelected(gameSwitch,attackingType)
-                makeVisibleIfTypeSelected(iceJiceSwitch,attackingType)
-                makeVisibleIfTypeSelected(tableHeader,attackingType)
-
+                adjustTableHeaderText()
+                makeGridAndSwitchesVisibleIfATypeIsSelected()
                 refreshTheData()
             }
             override fun onNothingSelected(p0: AdapterView<*>?) {}
@@ -186,18 +163,9 @@ class MainActivity : AppCompatActivity() {
             override fun onItemSelected(p0: AdapterView<*>, p1: View, p2: Int, p3: Long) {
                 defSpinner1Index = p2
                 defendingType1 = Types.values()[p2].type
-
-                // Table header text adjustment
-                adjustTableHeaderText(tableHeader,defendingType1,defendingType2)
-
-                // Adjusts visibility depending on whether user has selected a type yet
+                adjustTableHeaderText()
+                makeGridAndSwitchesVisibleIfATypeIsSelected()
                 doesNotExistDisclaimer.visibility = if (mainActivityViewModel.doesThisTypingExist(defendingType1,defendingType2)) {View.VISIBLE} else {View.INVISIBLE}
-                makeVisibleIfTypeSelected(tableHeader,defendingType1,defendingType2)
-                makeVisibleIfTypeSelected(typeTableRecyclerView,defendingType1,defendingType2)
-                makeVisibleIfTypeSelected(gameSwitchText,defendingType1,defendingType2)
-                makeVisibleIfTypeSelected(gameSwitch,defendingType1,defendingType2)
-                makeVisibleIfTypeSelected(iceJiceSwitch,defendingType1,defendingType2)
-
                 refreshTheData()
             }
             override fun onNothingSelected(p0: AdapterView<*>?) {}
@@ -207,32 +175,19 @@ class MainActivity : AppCompatActivity() {
             override fun onItemSelected(p0: AdapterView<*>, p1: View, p2: Int, p3: Long) {
                 defSpinner2Index = p2
                 defendingType2 = Types.values()[p2].type
-
-                // Table header text adjustment
-                adjustTableHeaderText(tableHeader,defendingType1,defendingType2)
-
-                // Adjusts visibility depending on whether user has selected a type yet
+                adjustTableHeaderText()
+                makeGridAndSwitchesVisibleIfATypeIsSelected()
                 doesNotExistDisclaimer.visibility = if (mainActivityViewModel.doesThisTypingExist(defendingType1,defendingType2)) {View.VISIBLE} else {View.INVISIBLE}
-                makeVisibleIfTypeSelected(tableHeader,defendingType1,defendingType2)
-                makeVisibleIfTypeSelected(typeTableRecyclerView,defendingType1,defendingType2)
-                makeVisibleIfTypeSelected(gameSwitchText,defendingType1,defendingType2)
-                makeVisibleIfTypeSelected(gameSwitch,defendingType1,defendingType2)
-                makeVisibleIfTypeSelected(iceJiceSwitch,defendingType1,defendingType2)
-
                 refreshTheData()
             }
-
             override fun onNothingSelected(p0: AdapterView<*>?) {}
         }
 
         gameSwitchText.setOnClickListener { gameSwitch.toggle() } // toggles gameSwitch when text is clicked
         gameSwitch.setOnCheckedChangeListener { _, onSwitch ->
-
-            // Changes the boolean and adjusts textViews
             mainActivityViewModel.pogoTime = onSwitch
             gameSwitchText.text = if (mainActivityViewModel.pogoTime) {
                 resources.getString(R.string.pogo) } else { resources.getString(R.string.mainGame)}
-
             refreshTheData()
         }
 
@@ -250,12 +205,8 @@ class MainActivity : AppCompatActivity() {
             // Adjusts the text in the table header (only if Ice/Jice is currently selected)
             if ((attackingType == "Ice" || defendingType1 == "Ice" || defendingType2 == "Ice")
                 || (attackingType == "Jice" || defendingType1 == "Jice" || defendingType2 == "Jice")) {
-                when (weAreDefending) {
-                    false -> adjustTableHeaderText(tableHeader, attackingType)
-                    true -> adjustTableHeaderText(tableHeader, defendingType1, defendingType2)
-                }
+                adjustTableHeaderText()
             }
-
             refreshTheData()
         }
         infoButton.setOnClickListener {
@@ -268,12 +219,23 @@ class MainActivity : AppCompatActivity() {
     /////////////////////////////////     End of onCreate     ///////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////////////
 
-    private fun makeVisibleIfTypeSelected(givenView: View, type1: String, type2: String = "[none]") {
-        if ((type1 == "(choose)" || type1 == Types.NoType.type) && (type2 == "[none]" || type2 == Types.NoType.type)) {
-            givenView.visibility = View.INVISIBLE
-        } else {
-            givenView.visibility = View.VISIBLE
-        }
+    private fun makeGridAndSwitchesVisibleIfATypeIsSelected() {
+        val type1 = if (weAreDefending) { defendingType1 } else { attackingType }
+        val type2 = if (weAreDefending) { defendingType2 } else { "[none]" }
+                if ((type1 == "(choose)" || type1 == Types.NoType.type) &&
+                    (type2 == "[none]" || type2 == Types.NoType.type)) {
+                    typeTableRecyclerView.visibility = View.INVISIBLE
+                    gameSwitchText.visibility = View.INVISIBLE
+                    gameSwitch.visibility = View.INVISIBLE
+                    iceJiceSwitch.visibility = View.INVISIBLE
+                    tableHeader.visibility = View.INVISIBLE
+                } else {
+                    typeTableRecyclerView.visibility = View.VISIBLE
+                    gameSwitchText.visibility = View.VISIBLE
+                    gameSwitch.visibility = View.VISIBLE
+                    iceJiceSwitch.visibility = View.VISIBLE
+                    tableHeader.visibility = View.VISIBLE
+                }
     }
 
     // UI
@@ -286,7 +248,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     // UI
-    private fun adjustTableHeaderText(tableHeader: TextView, type1: String, type2: String = "[none]") {
+    private fun adjustTableHeaderText() {
+        val type1 = if (weAreDefending) { defendingType1 } else { attackingType }
+        val type2 = if (weAreDefending) { defendingType2 } else { "[none]" }
         if ((type1 == "(choose)" || type1 == Types.NoType.type) && (type2 == "[none]" || type2 == Types.NoType.type)){
             tableHeader.visibility = View.GONE
         } else {tableHeader.visibility = View.VISIBLE}
@@ -302,45 +266,25 @@ class MainActivity : AppCompatActivity() {
         when (weAreDefending) {
             true -> {
                 if ((type1Displayed != "(choose)" && type1Displayed != Types.NoType.type) && (type2Displayed == "[none]" || type2Displayed == Types.NoType.type)) {
-                    tableHeader.text = resources.getString(
-                        R.string.table_header_one_type,
-                        "_____",
-                        type1Displayed
-                    )
+                    tableHeader.text = resources.getString(R.string.table_header_one_type, "_____", type1Displayed)
                 }
                 if ((type1Displayed == "(choose)" || type1Displayed == Types.NoType.type) && (type2Displayed != "[none]" && type2Displayed != Types.NoType.type)) {
-                    tableHeader.text = resources.getString(
-                        R.string.table_header_one_type,
-                        "_____",
-                        type2Displayed
-                    )
+                    tableHeader.text = resources.getString(R.string.table_header_one_type, "_____", type2Displayed)
                 }
                 if ((type1Displayed != "(choose)" && type1Displayed != Types.NoType.type) && (type2Displayed != "[none]" && type2Displayed != Types.NoType.type) && type1Displayed != type2Displayed) {
-                    tableHeader.text = resources.getString(
-                        R.string.table_header_two_types,
-                        "_____",
-                        type1Displayed,
-                        type2Displayed
-                    )
+                    tableHeader.text = resources.getString(R.string.table_header_two_types, "_____", type1Displayed, type2Displayed)
                 }
                 if ((type1Displayed != "(choose)" && type1Displayed != Types.NoType.type) && type1Displayed == type2Displayed) {
-                    tableHeader.text = resources.getString(
-                        R.string.table_header_one_type,
-                        "_____",
-                        type1Displayed
-                    )
+                    tableHeader.text = resources.getString(R.string.table_header_one_type, "_____", type1Displayed)
                 }
             }
             false -> {
-                tableHeader.text = resources.getString(
-                    R.string.table_header_one_type,
-                    type1Displayed, "_____"
-                )
+                tableHeader.text = resources.getString(R.string.table_header_one_type, type1Displayed, "_____")
             }
         }
     }
 
-    // BL to UI
+    // BL BUT IT USES THE TWO FUNCTIONS THAT NEED COLORS
     private fun interactionsToGridView(interactionsList: MutableList<Double>) {
         val effectivenessList = mainActivityViewModel.interactionsToEffectiveness(interactionsList)
         val displayedListOfInteractions = mainActivityViewModel.effectivenessToDisplayedCellValues(effectivenessList)
@@ -351,8 +295,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView?.adapter = typeGridAdapter
     }
 
-    // BL
-    // NEEDS COLORS
+    // BL BUT NEEDS COLORS, which I can't get in MainActivityViewModel yet
     private fun effectivenessToCellTextColors(mutableList: MutableList<String>): MutableList<Int> {
         val listOfCellTextColors: MutableList<Int> = mutableListOf()
         for (i in 0 until 18) {
@@ -365,8 +308,7 @@ class MainActivity : AppCompatActivity() {
         return listOfCellTextColors
     }
 
-    // BL
-    // NEEDS COLORS
+    // BL BUT NEEDS COLORS, which I can't get in MainActivityViewModel yet
     private fun effectivenessToCellBackgroundColors(mutableList: MutableList<String>): MutableList<Int> {
         val listOfCellBackgroundColors: MutableList<Int> = mutableListOf()
         for (i in 0 until 18) {
@@ -383,6 +325,7 @@ class MainActivity : AppCompatActivity() {
         return listOfCellBackgroundColors
     }
 
+    // Strikes me as BL, but I don't want to call in attackingType, defendingType1, defendingType2 from MainActivity
     private fun refreshTheData() {
         when (weAreDefending) {
             false -> {
@@ -406,7 +349,7 @@ class MainActivity : AppCompatActivity() {
         interactionsToGridView(mainActivityViewModel.listOfInteractions)
     }
 
-    // BL
+    // BL, to be implemented later when LiveData is a thing
     /*private fun justChangeJiceInGridView() {
         arrayListForTypeGrid?.get(11)?.iconsInGridView = if (mainActivityViewModel.jiceTime) { R.drawable.jice_icon } else { R.drawable.ice_icon }
         arrayListForTypeGrid?.let { TypeGridAdapter(it).submitList(arrayListForTypeGrid) }
